4cfb3b804df01a88a66f6b079deae963ebd03bb7
SaiShruthi <saishruthi.t@techouts.com>
2022-03-14 15:39:02 +0530

    NestedifExample

---
+++ Nestedif/Nested.java
@@ -0,0 +1,27 @@
+package Nestedif;
+
+public class Nested
+{
+	public static void main(String[] args)
+	{
+		
+		String str1 = new String("Goa");
+	    String str2 = "Gowthami";
+	    String str3 = str1.intern();
+		System.out.println(str2);
+		String city="Hyderabad";
+		if(city=="Hyderabad")
+		{
+			System.out.println("Your city is Hyderabad");
+		
+		if(city=="Noida") {
+			System.out.println("Your city is Noida");
+		}
+		else
+		{
+			System.out.println("Your city is Vijayawada");
+		}
+		}
+		
+	}
+}
