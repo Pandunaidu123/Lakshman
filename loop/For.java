e197b9d79af6bdf04ee53f83f1e182b347055b4f
SaiShruthi <saishruthi.t@techouts.com>
2022-03-14 16:41:26 +0530

    Fruit

---
+++ loop/For.java
@@ -0,0 +1,22 @@
+package loop;
+
+import java.util.*;
+import java.io.*;
+public class For {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the range");
+		int n=sc.nextInt();
+		System.out.println("Enter the range");
+		//int i=sc.nextInt();
+	    for(int i=3;i<n+3;i=i+3)
+	    {
+	       System.out.println(i);
+	    }
+		
+		
+
+	}
+
+}
