f385b4da2dff19eff1de98581c243c27486ea295
SaiShruthi <saishruthi.t@techouts.com>
2022-03-14 16:22:55 +0530

    Bro

---
+++ sw/Switch.java
@@ -0,0 +1,32 @@
+package sw;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		int a=30;
+		int marks=50;
+		String s=new String("Mumbai");
+	    switch(a)
+		{
+			case 10:
+				System.out.println("India is the Developing country");
+				break;
+			case 20:
+				System.out.println("This is 20");
+				break;
+			case 30:
+				switch(marks)
+				{
+			case 50:
+				System.out.println("This is 30");
+				break;
+		default:
+				System.out.println("Hyderabad");
+		
+		}
+		}
+		
+
+	}
+
+}
