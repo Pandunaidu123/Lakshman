8e935fad529db6ee9cb4e9c7900a56ffed51626b
SaiShruthi <saishruthi.t@techouts.com>
2022-03-15 15:29:40 +0530

    Class

---
+++ ioop2/Class.java
@@ -0,0 +1,16 @@
+package ioop2;
+
+class Class{  
+ 
+	 int id=120; 
+	 float id1=(int)120; 
+	 String name="Techouts Solutions Pvt Ltd., Gachibowli, Hyderabad...!!"; 
+	 String name2="Techouts Hiriung studets from NMREC, HYD";
+	 public static void main(String args[]){  
+	  Class s1=new Class();  
+	  System.out.println(s1.id);
+	  System.out.println(s1.id1);
+	  System.out.println(s1.name);  
+	  System.out.println(s1.name2); 
+	 }  
+	}  
