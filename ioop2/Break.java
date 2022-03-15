200865f1e22281b9f0c52bb3da0c7d5cafe644a0
SaiShruthi <saishruthi.t@techouts.com>
2022-03-15 15:14:07 +0530

    Hlo

---
+++ ioop2/Break.java
@@ -0,0 +1,18 @@
+package ioop2;
+
+public class Break
+{ 
+	public static void main(String[] args)
+		{  
+		  
+		    for(int i=1;i<=10;i++){  
+		        if(i==7){  
+		           
+		            break;  
+		        }  
+		        System.out.println(i);  
+		    }  
+		}  
+		}  
+
+
