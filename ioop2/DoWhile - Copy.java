e2d75b0b4322a8d384c402e3eec0923d922b83d2
SaiShruthi <saishruthi.t@techouts.com>
2022-03-15 15:58:54 +0530

    Adding Extra Copy

---
+++ ioop2/DoWhile - Copy.java
@@ -0,0 +1,17 @@
+package ioop2;
+
+public class DoWhile
+{
+	public static void main(String[] args) {
+	    String name="SidduNaidu";
+	    boolean bool=true;
+	    do
+	    {
+	        System.out.println(name);
+	        bool=false;
+	     }while(bool);
+	    
+	    
+	}
+}
+
