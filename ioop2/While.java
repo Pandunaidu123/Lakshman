1a949b7571f58b37a51b89c61dae32a81a9675af
SaiShruthi <saishruthi.t@techouts.com>
2022-03-15 13:43:11 +0530

    fruit

---
+++ ioop2/While.java
@@ -0,0 +1,17 @@
+package ioop2;
+
+public class While
+{
+	public static void main(String[] args) {
+	    String name="SidduNaidu";
+	    boolean bool=true;
+	    while(bool)
+	    {
+	        System.out.println(name);
+	        bool=false;
+	    }
+	    
+	    
+	}
+}
+
