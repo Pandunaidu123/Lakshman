18266400857ed95b336ed8cd5531fc9bde6e6960
SaiShruthi <saishruthi.t@techouts.com>
2022-03-15 13:33:13 +0530

    arithmeticEx

---
+++ ioop2/Arithmetic.java
@@ -0,0 +1,16 @@
+package ioop2;
+
+public class Arithmetic
+{
+	public static void main(String[] args) {
+	    int a=10;
+	    int b=30;
+	    int c=a+b;
+	    System.out.println(c);
+		System.out.println("Hello World");
+		System.out.println(c=c-b);
+		System.out.println(c=c*b);
+		
+	}
+}
+
