3fb281d5f26373ad8fa7059cceaeac03cc6a32d2
SaiShruthi <saishruthi.t@techouts.com>
2022-03-15 15:48:47 +0530

    DefaultConstructor

---
+++ ioop2/DefaultConstructor.java
@@ -0,0 +1,19 @@
+package ioop2;
+
+class Default_constructor{
+int id;
+String name;
+
+void display()
+{
+	System.out.println(id+" "+name);
+}
+
+public static void main(String args[])
+{
+	Default_constructor s1=new Default_constructor();
+	Default_constructor s2=new Default_constructor();
+s1.display();
+s2.display();
+}
+}
